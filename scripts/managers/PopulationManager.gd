3697df61b78cc841615af61fea5d7a70d11f48cb
Leonardo Monteblanco <leonardomonteblanc@gmail.com>
2026-05-31 15:18:20 -0300

    feat(task-25): implementa geração de IDs únicos

--- scripts/managers/PopulationManager.gd
+++ scripts/managers/PopulationManager.gd
@@ -4,11 +4,13 @@ extends Node
 const CENA_AGENTE = preload("res://scenes/agents/Agente.tscn")
 
 var agentes: Array = []
+var proximo_id: int = 0
 
 func criar_agentes():
 	for i in range(50):
 		var agente = CENA_AGENTE.instantiate()
-	
+		
+		agente.id = gerar_id_unico()
 		var margem = Agente.TAMANHO_AGENTE / 2.0
 		agente.position = Vector2(
 			randi_range(margem, Constants.LARGURA_AREA - margem),
@@ -17,7 +19,12 @@ func criar_agentes():
 		
 		agentes.append(agente)
 		get_tree().current_scene.add_child(agente)
+		print(agente.id)
 
+func gerar_id_unico() -> int:
+	proximo_id += 1
+	return proximo_id
+	
 
 # manipulacao da array com os agentes
 func adicionar_agente(agente):
