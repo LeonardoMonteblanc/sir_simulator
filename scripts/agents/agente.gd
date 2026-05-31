ba128c4cfdb8218f35ed332ef267001bd5387b93
Leonardo Monteblanco <leonardomonteblanc@gmail.com>
2026-05-31 14:45:25 -0300

    feat(task-18): implementa método set_state

--- scripts/agents/agente.gd
+++ scripts/agents/agente.gd
@@ -8,6 +8,11 @@ var direcao: Vector2 = Vector2.ZERO
 var tempo_infeccao: float = 0.0
 
 
+# metodo responsavel por alterar o estado 
+func set_estado(novo_estado: sir_estados.Estado) -> void:
+	estado = novo_estado
+	
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
