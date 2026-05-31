2eb87fd35cbf62e3798a95e7661dda073d779ee1
Leonardo Monteblanco <leonardomonteblanc@gmail.com>
2026-05-31 14:27:50 -0300

    feat(task-08): adiciona constantes globais da simulação

---
+++ scripts/globals/Constants.gd
@@ -0,0 +1,17 @@
+class_name Constants
+
+const FPS: int = 30
+
+const LARGURA_AREA: int = 800
+const ALTURA_AREA: int = 600
+
+const QTD_AGENTES: int = 50
+const QTD_INFECTADOS_INICIAL: int = 1
+
+const VELOCIDADE_AGENTE: float = 40.0
+
+const RAIO_CONTAGIO: float = 48.0
+
+const COR_SUSCETIVEL := Color.GREEN
+const COR_INFECTADO := Color.RED
+const COR_RECUPERADO := Color.BLUE
