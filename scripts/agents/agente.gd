06798ce8229a819a13c3917274ff8fa1206c664a
Leonardo Monteblanco <leonardomonteblanc@gmail.com>
2026-05-31 15:27:18 -0300

    feat(task-29): implementa movimentação contínua dos agentes

--- scripts/agents/agente.gd
+++ scripts/agents/agente.gd
@@ -47,4 +47,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	position += direcao * velocidade * delta
