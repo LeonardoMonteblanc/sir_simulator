dd01131de10b07480bcfcb042ef612488c03607e
Leonardo Monteblanco <leonardomonteblanc@gmail.com>
2026-06-22 18:42:49 -0300

    chore: deletando arquivos não usados/testes

--- scripts/tmp_load_main.gd
+++ scripts/tmp_load_main.gd
@@ -1,6 +1,6 @@
 extends SceneTree
 func _init():
-    var scene = load("res://scenes/mainSimulation.tscn")
-    var inst = scene.instantiate()
-    print("children:", inst.get_child_count())
-    quit()
+	var scene = load("res://scenes/mainSimulation.tscn")
+	var inst = scene.instantiate()
+	print("children:", inst.get_child_count())
+	quit()
